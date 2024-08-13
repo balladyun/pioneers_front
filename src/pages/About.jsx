5e29226cd652b67332c8f5c82bd8915b956688a1
Heeseung Ha <ssigu69@gmail.com>
2024-08-13 20:57:38 +0900

    소개 페이지 줄글 정렬 수정

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -45,29 +45,22 @@ export default function About() {
           <h4 className='mb-3 text-center text-title'>첫번째</h4>
           <p>
             <span className='font-bold'>창업팀</span>에 속한 여러분,{' '}
-            <span className='font-bold'>대화</span>가 필요해 보여요.
-          </p>
-          <p>
+            <span className='font-bold'>대화</span>가 필요해 보여요.{' '}
             <span className='font-bold'>동료와 함께 같은 목표를 설정</span>해 보세요!
           </p>
         </article>
         <article className={`relative z-10 ${article}`}>
           <h4 className='mb-3 text-center text-title'>두번째</h4>
           <p>
-            <span className='font-bold'>초기 입문 단계인 창업팀</span> 여러분,
-          </p>
-          <p>질문에 답하고 기록하며</p>
-          <p>
-            <span className='font-bold'>복잡한 머릿속을 정리</span>해 보세요!
+            <span className='font-bold'>초기 입문 단계인 창업팀</span> 여러분, 질문에 답하고
+            기록하며 <span className='font-bold'>복잡한 머릿속을 정리</span>해 보세요!
           </p>
         </article>
         <article className={`relative z-10 ${article}`}>
           <h4 className='mb-3 text-center text-title'>세번째</h4>
           <p>
-            <span className='font-bold'>팀원 또는 부원을 선발</span>할 때 적절한 질문카드를
-          </p>
-          <p>
-            활용하여 <span className='font-bold'>면접을 성사</span>시키세요!
+            <span className='font-bold'>팀원 또는 부원을 선발</span>할 때 적절한 질문카드를 활용하여{' '}
+            <span className='font-bold'>면접을 성사</span>시키세요!
           </p>
         </article>
       </section>
